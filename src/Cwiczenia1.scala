c71a2589f1129f04e72aeb642c1518b40ce579c4
Piotr Wezgraj <s13846@pjwstk.edu.pl>
2020-10-26 19:11:13 +0100

    Tasks 2-6

--- src/Cwiczenia1.scala
+++ src/Cwiczenia1.scala
@@ -1,14 +1,95 @@
+import scala.::
+import scala.annotation.tailrec
+
 object Cwiczenia1 {
   val dniTygodnia = List("Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela")
 
+  //1a
   def dniFor(dni: List[String]): String = {
     var out = ""
-    for (d <- dniTygodnia) out += d + ", "
-    out
+    for (d <- dni) out += d + ", "
+    out.dropRight(2)
   }
+  //1b
+  def dniForNaP(dni: List[String]): String = {
+    var out = List[String]()
+    for (d <- dni) if(d.toUpperCase().startsWith("P")) out = d::out
+    dniFor(out)
+  }
+  //1c
+  def dniWhile(dni: List[String]): String = {
+    var out = ""
+    var i: Int = 0
+    while(i < dni.length){
+      out += dni(i) + ", "
+      i+=1
+    }
+    out.dropRight(2)
+  }
+  //2a
+  def dniRek[T](dni: List[T]): String = dni match {
+    case Nil => ""
+    case _ => dni.head + ", " + dniRek(dni.tail)
+  }
+  //2b
+  def dniRekWst[T](dni: List[T]): String = dni match {
+    case Nil => ""
+    case _ => dniRekWst(dni.tail) + ", " + dni.head
+  }
+  //3
+  def dniTail[T](dni: List[T]): String = {
+    @tailrec
+    def appendStr[T](dni: List[T], str: String): String = dni match {
+      case Nil => str.dropRight(2)
+      case head :: tail => appendStr(tail, str + head + ", ")
+    }
+    appendStr(dni, "")
+  }
+  //4a
+  def dniFoldl(dni: List[String]): String = {
+    dni.foldLeft(""){ (acc, item) =>
+      if(acc.isEmpty) acc + item
+      else acc + ", " + item
+    }
+  }
+  //4b
+  def dniFoldr(dni: List[String]): String = {
+    dni.foldRight(""){ (acc, item) =>
+      if(item.isEmpty) acc + item
+      else acc + ", " + item
+    }
+
+  }
+  //4c
+  def dniFoldlP(list: List[String]): String = {
+    list.filter(_.toUpperCase()
+      .startsWith("P")
+    ) .foldLeft(""){ (acc, item) =>
+      if(acc.isEmpty) acc + item
+      else acc + ", " + item
+    }
+  }
+  //5
+  val produkty = Map (
+    "CPU" -> 20d,
+    "GPU" -> 30d,
+    "Płyta główna" -> 19.9d,
+    "Laptop gamingowy" -> 59.9d,
+    "Gra komputerowa o czołgach" -> 199d
+  )
+  val produktyRabat = produkty.transform((_, v) => v * 0.9)
+  //6
+  val krotka = ("kb", 3d, None)
+  def printKrotka[A, B, C](krotka: (A, B, C)): Unit = {
+    println(krotka._1 + " - " + krotka._2 + " - " + krotka._3)
+  }
+  //7
+
+  printKrotka(krotka)
   def main(args: Array[String]): Unit = {
     println("Hello, world")
-    println(dniFor(dniTygodnia))
+    println(dniForNaP(dniTygodnia))
+    println(dniWhile(dniTygodnia))
   }
 
 }
