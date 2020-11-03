b5ea6c0711d4a4562f76a6183dd90e6cbf51fd9d
Piotr Wezgraj <s13846@pjwstk.edu.pl>
2020-11-03 14:45:11 +0100

    Task 3 from Cwiczenia2
    Signed-off-by: Piotr Wezgraj <s13846@pjwstk.edu.pl>

--- src/Cwiczenia2.scala
+++ src/Cwiczenia2.scala
@@ -52,7 +52,36 @@ object Cwiczenia2 {
   println(kb2.toString)
   //kb1.stanKonta = 999 powoduje prawidłowo błąd jeszcze przed kompilacją, a kb1.wplata(-100) w czasie runtime
   //Zadanie 3
+  class Osoba(var imie: String, var nazwisko: String)
 
+  var osoba1 = new Osoba("Piotr", "Abacki")
+  var osoba2 = new Osoba("Jan", "Babacki")
+  var osoba3 = new Osoba("Anastasiia", "Cbacka")
+  var osoba4 = new Osoba("John", "Abacki")
+  var osoba5 = new Osoba("Jan", "Matejko")
+
+  def greetPerson(osoba: Osoba): String = osoba.nazwisko match {
+    case "Abacki" => osoba.imie match {
+      case "Piotr" => "Elo Piotrek, coś Ty taki zmęczony?"
+      case "John" => "Whats up bro?"
+      case _ => "Wiesz co "+osoba.imie+"? Znam Johna i Piotra. To Twoja rodzina?"
+    }
+    case "Babacki" => osoba.imie match {
+      case  "Jan" => "Ooo! Witam Pana Jana."
+      case _ => "Dzień dobry "+osoba.imie+". Jesteś kimś od Jana?"
+    }
+    case "Cbacka" => osoba.imie match{
+      case "Anastasiia" => "Cześć Anaastasiia ( ͡° ͜ʖ ͡°)"
+      case _ => "Witam, nie znam Cię "+ osoba.imie + "ale może znasz Anastasiię? To taka fajna dziewczyna."
+    }
+    case _ => "Dzień dobry " + osoba.imie + " " + osoba.nazwisko + ". Miło poznać taką wspaniałą osobę jak Ty."
+  }
+
+  println(greetPerson(osoba1))
+  println(greetPerson(osoba2))
+  println(greetPerson(osoba3))
+  println(greetPerson(osoba4))
+  println(greetPerson(osoba5))
 
 
   def main(args: Array[String]): Unit = {
